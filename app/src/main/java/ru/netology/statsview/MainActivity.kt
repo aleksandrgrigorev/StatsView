d02e7b363815259b24b48a6b8aa6ec4fadebe948
aleksandrgrigorev <xundr1992@gmail.com>
2023-03-07 14:22:56 +0400

    Rotation

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -1,9 +1,6 @@
 package ru.netology.statsview
 
 import android.os.Bundle
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.statsview.ui.StatsView
 
@@ -11,33 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val view = findViewById<StatsView>(R.id.statsView)
         view.data = listOf(
             500F,
             500F,
             500F,
             500F,
-        )
-
-        val textView = findViewById<TextView>(R.id.label)
-
-        view.startAnimation(
-            AnimationUtils.loadAnimation(this, R.anim.animation).apply {
-                setAnimationListener(object: Animation.AnimationListener {
-                    override fun onAnimationStart(animation: Animation?) {
-                        textView.text = "onAnimationStart"
-                    }
-
-                    override fun onAnimationEnd(animation: Animation?) {
-                        textView.text = "onAnimationEnd"
-                    }
-
-                    override fun onAnimationRepeat(animation: Animation?) {
-                        textView.text = "onAnimationRepeat"
-                    }
-
-                })
-            }
         )
     }
 }
